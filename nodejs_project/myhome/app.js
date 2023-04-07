317c38decc2531f5cf7f9a06f4e016bc78290932
eejeong <wdwaaa7325@gmail.com>
2023-04-07 17:37:09 +0900

    0407

--- nodejs_project/myhome/app.js
+++ nodejs_project/myhome/app.js
@@ -3,10 +3,15 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+const session = require("express-session");
+const MYSQLSTORE = require("express-mysql-session")(session);
+const DBInfo = require("./routes/commonDB"); //세션이 저장될 디비 정보
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let boardRouter = require('./routes/board');
+let memberRouter = require('./routes/member');
+const MySQLStore = require('express-mysql-session');
 
 let app = express();
 
@@ -14,15 +19,25 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//미들웨어 : 모든 웹 상의 요청이 거쳐가는 구역
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+console.log(DBInfo.DBInfo);
+let sessionStore=new MYSQLSTORE(DBInfo.DBInfo);
+app.use(session({
+  key:"session key",
+  secret:"dkaghghk", //암호화할 때 사용
+  store:sessionStore,
+  resave:false,
+  saveUninitialized:false
+}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/board', boardRouter);
+app.use('/member', memberRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
